e4fc4858bce6a3458acfd8d643e366bb372ffc60
insun4210@gmail.com <insun4210@gmail.com>
2024-11-01 15:00:59 +0900

    no message

---
+++ src/main/java/com/lyj/securitydomo/controller/AdminController.java
@@ -0,0 +1,14 @@
+package com.lyj.securitydomo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    @GetMapping("/admin_index")
+    public String list() {
+        return "admin/admin_index";  // 뷰 이름을 문자열로 반환
+    }
+}
