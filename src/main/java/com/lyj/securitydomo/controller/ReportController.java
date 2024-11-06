0cea276c37f7fbf219e036a4b2a37601cd3335e5
insun4210@gmail.com <insun4210@gmail.com>
2024-11-06 10:07:16 +0900

    no message

--- src/main/java/com/lyj/securitydomo/controller/ReportController.java
+++ src/main/java/com/lyj/securitydomo/controller/ReportController.java
@@ -28,10 +28,10 @@ public class ReportController {
         logger.info(reportDTO.toString());
 
         // 로그 찍기
-        logger.info("신고 생성 요청: ",reportDTO);
-
-        createReport(reportDTO);
-        logger.info("신고가 성공적으로 접수되었습니다.");
+//        logger.info("신고 생성 요청: ",reportDTO);
+//
+//        createReport(reportDTO);
+//        logger.info("신고가 성공적으로 접수되었습니다.");
 
         return ResponseEntity.ok("신고가 접수되었습니다."); // 여기서 문자열 응답
     }
